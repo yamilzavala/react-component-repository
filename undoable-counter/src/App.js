7b692c7e2aebcf79052235e6bafcd8632f543afa
yamilzavala <zavalayamil9@gmail.com>
2025-02-20 15:18:45 -0300

    undo operations

---
+++ undoable-counter/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import UndoableCounter from "./components/UndoableCounter";
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <UndoableCounter />
+    </div>
+  );
+}
